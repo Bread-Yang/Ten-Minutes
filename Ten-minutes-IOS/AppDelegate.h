40c05f7c0ad38655b59b7b3a3b6778e326831026
杨镕宾 <rbyang@xtremeprog.com>
2014-10-02 13:36:59 +0800

    Initial Commit

---
+++ Ten-minutes-IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ten-minutes-IOS
+//
+//  Created by yoghourt on 10/2/14.
+//  Copyright (c) 2014 genio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ten-minutes-IOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ten-minutes-IOS
+//
+//  Created by yoghourt on 10/2/14.
+//  Copyright (c) 2014 genio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
